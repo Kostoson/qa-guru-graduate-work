e0ab768143d6d27c6f19434bb4b0cb5871d5fdd5
Konstantin Dubinin <129156388+Kostoson@users.noreply.github.com>
2023-07-05 21:47:50 +0300

    Update BaseTest.java

--- src/test/java/web/tests/BaseTest.java
+++ src/test/java/web/tests/BaseTest.java
@@ -25,8 +25,6 @@ public class BaseTest {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true, //чтобы отображалось в селенойде, что внутри просиходит
